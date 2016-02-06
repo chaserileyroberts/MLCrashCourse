f44d2ccb674100031bd8579445200817bf433ce6
Thenerdstation <keeper6928@gmail.com>
2016-02-06 12:25:25 -0500

    Added example code

--- Ex1.py
+++ Ex1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import MachineLearningTools as mlt
 
-paitentData = np.array([
+paitentData = np.matrix([
                [1, -.5], 
                [1,  .3], 
                [1,  .24], 
@@ -11,11 +11,24 @@ paitentData = np.array([
               ])
 hasCancerData = [-1, -1, -1, 1, 1, 1]
 def main():
-    w = np.array([0, 0])
+    w = np.matrix([0., 0.])
+    index = 0
+    while True:
+      if (w.dot(paitentData[index].T) > 0) != ( hasCancerData[index] > 0):
+        w += paitentData[index] * hasCancerData[index]
+        index = 0
+        continue
+      index += 1
+      if index >= len(hasCancerData):
+        break
+    print w.tolist()[0]
+
+
+
     #### Do the training here ###
 
 
-    mlt.plot_prec(paitentData, hasCancerData, w)
+    mlt.plot_prec(paitentData.tolist(), hasCancerData, w.tolist()[0])
     
 
 if __name__ == '__main__':
